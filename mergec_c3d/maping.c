a2aa36e82004665f87daf380eda8b85657e38e94
mlameira <mlameira@student.42lisboa.com>
2025-06-24 13:37:39 +0000

    merge done

---
+++ mergec_c3d/maping.c
@@ -0,0 +1,51 @@
+#include "cub3d.h"
+
+int	getmap(char *filename, t_game *vars)
+{
+	char	*hld;
+	char	*maphld;
+	int		fd;
+
+	(void)vars;
+	fd = open(filename, O_RDONLY);
+	if (fd < 1)
+		return (0);
+	hld = get_next_line(fd);
+	maphld = NULL;
+	while (hld)
+	{
+		if (hld[0] == '\n')
+			return (free(hld), free(maphld), 0);
+		maphld = get_lines(maphld, hld, '\0');
+		free(hld);
+		hld = get_next_line(fd);
+	}
+	if (!ft_strrchr(maphld, 'P'))
+		return (close(fd), free(maphld), 0);
+	vars->map = NULL;
+	vars->map = ft_split(maphld, '\n');
+	free(maphld);
+	close(fd);
+	return (1);
+}
+
+int	getplayerpos(t_game *vars)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	printf("hi\n");
+	while(vars->map[++i])
+	{
+		j = -1;
+		while (vars->map[i][++j])
+			if (vars->map[i][j] == 'P')
+				{
+					vars->x = j;
+					vars->y = i;
+					return 0;
+				}
+	}
+	return 1;
+}
